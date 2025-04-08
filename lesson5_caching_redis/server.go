33120c444f02a9ee84fd8a2473eacc04947f9630
trinhmn49 <ngoctrinhmai040920@gmail.com>
2025-04-09 00:04:16 +0700

    add basic login and get info

--- lesson5_caching_redis/server.go
+++ lesson5_caching_redis/server.go
@@ -1,26 +1,54 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	redis "github.com/go-redis/redis/v8"
+	"time"
 )
 
 var (
-	ip = "localhost"
+	ip   = "localhost"
 	port = "6379"
 )
 
-contextRedis := context.Background()
-redisClient := redis.NewClient(
+var contextRedis = context.Background()
+var redisClient = redis.NewClient(
 	&redis.Options{
-		Addr: ip+":"+port, //todo: need change
-		Password: "",				//password redis
-		DB: 0,					//db redis				
-	}
+		Addr:     ip + ":" + port, //todo: need change
+		Password: "",              //password redis
+		DB:       0,               //db redis
+	},
 )
 
 type user struct {
-	username string `json:"username"`
-	sessionId string `json:"sessionId"`
+	Username  string `json:"username"`
+	SessionId string `json:"sessionId"`
+}
+
+func login(c *gin.Context) {
+	username := c.Query("username")
+	sessionId := c.Query("sessionId")
+	err := redisClient.Set(contextRedis, sessionId, username, time.Hour).Err()
+	if err != nil {
+		fmt.Println("redis set error", err)
+		return
+	}
+}
+
+func getUserInfo(c *gin.Context) {
+	key := c.Query("sessionId")
+	value, err := redisClient.Get(contextRedis, key).Result()
+	if err != nil {
+		fmt.Println("redis get error", err)
+		return
+	} else if errors.Is(err, redis.Nil) {
+		fmt.Printf("Key '%s' does not exist\n", key)
+	} else {
+		fmt.Printf("value: '%s' for key: '%s'", key, value)
+	}
 }
 
 func main() {
@@ -31,6 +59,17 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "login",
 		})
+		login(c)
 	})
-	r.Run(":8080")
+
+	groupApi.GET("/show-info", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "show data of redis",
+		})
+		getUserInfo(c)
+	})
+	err := r.Run(":8080")
+	if err != nil {
+		return
+	}
 }
